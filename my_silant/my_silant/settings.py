f68cf5fec2084298759944553eea124403362ff2
roman7586 <1xcanonx1>
2023-03-08 11:38:17 +0300

    аутентификация рабочая

--- my_silant/my_silant/settings.py
+++ my_silant/my_silant/settings.py
@@ -128,7 +128,10 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-LOGIN_URL = '/accounts/login/'
+LOGIN_URL = '/accounts/login/' # Авторизация через allauth
+LOGIN_REDIRECT_URL = '/cars/' # перенаправление на страницу для авторизованного пользователя
+ACCOUNT_LOGOUT_REDIRECT_URL ="/accounts/login/" #перенаправление на страницу авторизации после выхода их профиля
+
 SITE_ID = 1
 ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_UNIQUE_EMAIL = True
@@ -141,3 +144,11 @@ ACCOUNT_ADAPTER = 'silant.adapter.CustomAccountAdapter'
 # A custom variable we created to tell the CustomAccountAdapter whether to
 # allow signups.
 ACCOUNT_ALLOW_SIGNUPS = False
+
+AUTHENTICATION_BACKENDS = [
+    # Needed to login by username in Django admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
+
+    # `allauth` specific authentication methods, such as login by e-mail
+    'allauth.account.auth_backends.AuthenticationBackend',
+]
