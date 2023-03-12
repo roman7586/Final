07f165890860949d4b51e74e4580ccf4629f98e8
roman7586 <1xcanonx1>
2023-03-12 19:05:24 +0300

    рефакторинг фильтров

--- my_silant/silant/filters.py
+++ my_silant/silant/filters.py
@@ -1,17 +1,25 @@
-from .models import Car, Complaints, Maintenance
-from django_filters import FilterSet
+from .models import Car, Complaints, Maintenance, Technique_model
+from django_filters import FilterSet, CharFilter, ModelChoiceFilter
+from django import forms
+
 
 class CarFilter(FilterSet):
-   class Meta:
-       model = Car
+    technique_model = ModelChoiceFilter(queryset=Technique_model.objects.all(), field_name='technique_model',label='Модель техники', widget=forms.Select(attrs={"class":"form-control text-black text-center","max_length":"100"}))     
+    serial_number = CharFilter(field_name='serial_number',label='Серийный номер', lookup_expr='icontains', widget=forms.TextInput(attrs={"class":"form-control text-black text-center","max_length":"100"}))
 
-       fields = {
-            'technique_model__name': ['icontains'],
-            'engine_model__name': ['icontains'],
-            'transmission_model__name': ['icontains'],
-            'drive_axle_model__name': ['icontains'],
-            'steerable_axle_model__name': ['icontains']
-            }
+    class Meta:
+         model = Car
+         fields = {
+             'serial_number', 'technique_model'
+             }
+
+    #    fields = {
+    #         'technique_model__name': ['icontains'],
+    #         'engine_model__name': ['icontains'],
+    #         'transmission_model__name': ['icontains'],
+    #         'drive_axle_model__name': ['icontains'],
+    #         'steerable_axle_model__name': ['icontains']
+    #         }
        
 class CarFilterNoAut(FilterSet):
    class Meta:
