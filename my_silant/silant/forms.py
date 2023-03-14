e3049d6e2c9fbc885bbdba3afbdd5002b898e69d
roman7586 <roman7586@gmail.com>
2023-03-14 14:42:25 +0300

    "красивая" форма для создания ТО

--- my_silant/silant/forms.py
+++ my_silant/silant/forms.py
@@ -1,6 +1,6 @@
 
 from django import forms
-from .models import Car, Client, Complaints, Drive_axle_model, Engine_model, Maintenance, Service_company, Steerable_axle_model, Technique_model, Transmission_model
+from .models import Car, Client, Complaints, Drive_axle_model, Engine_model, Maintenance, Organization_maintenance, Service_company, Steerable_axle_model, Technique_model, Transmission_model, Type_maintenance
 from django.utils import timezone
 
 now = timezone.now()
@@ -30,11 +30,16 @@ class CreateCarForm(forms.ModelForm):
 
 
 class CreateMaintenanceForm(forms.ModelForm):
+    type_maintenance = forms.ModelChoiceField(queryset=Type_maintenance.objects.all(), label='Вид ТО', widget=forms.Select(attrs={"class":"form-control text-black text-center"}))
+    organization_maintenance = forms.ModelChoiceField (queryset=Organization_maintenance.objects.all(), label='Организация, проводившая ТО', widget=forms.Select(attrs={"class":"form-control text-black text-center"}))
+    car = forms.ModelChoiceField (queryset=Car.objects.all(), label='Машина', widget=forms.Select(attrs={"class":"form-control text-black text-center"}))
+    service_company = forms.ModelChoiceField (queryset=Service_company.objects.all(), label='Сервисная организация', widget=forms.Select(attrs={"class":"form-control text-black text-center"}))
+    operating_time = forms.IntegerField (min_value='0', label='Наработка м/час', widget=forms.NumberInput (attrs={"class":"form-control text-black text-center"}))
     class Meta:
         model = Maintenance
-        widgets = {'maintenance_date': forms.SelectDateWidget(years=list(reversed(range(2000, now.year+1)))),
-                   'work_order': forms.Textarea(attrs={'rows': 1}),
-                   'date_work_order': forms.SelectDateWidget(years=list(reversed(range(2000, now.year+1)))),
+        widgets = {'maintenance_date': forms.SelectDateWidget(years=list(reversed(range(2000, now.year+1))), attrs={"rows": 1,"class":"form-control text-black text-center"}),
+                   'work_order': forms.Textarea(attrs={"rows": 1,"class":"form-control text-black text-center"}),
+                   'date_work_order': forms.SelectDateWidget(years=list(reversed(range(2000, now.year+1))), attrs={"rows": 1,"class":"form-control text-black text-center"}),
         }
         fields = '__all__'
 
