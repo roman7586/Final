878b895964e046a03740771a2a6027e8d298786e
roman7586 <1xcanonx1>
2023-03-15 20:43:34 +0300

    ТО и рекламация,показ зависит от прав видимости

--- my_silant/api/urls.py
+++ my_silant/api/urls.py
@@ -4,10 +4,12 @@ from .views import *
 
 router = routers.DefaultRouter()
 router.register(r'cars', CarViewSet)
-router.register(r'maintenance', MaintenanceViewSet)
-router.register(r'complaints', ComplaintsViewSet)
+#router.register(r'maintenance', MaintenanceViewSet)
+#router.register(r'complaints', ComplaintsViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('complaints/', ComplaintsList.as_view(), name='complaints-list'),
+    path('maintenances/', MaintenanceList.as_view(), name='maintenances-list'),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
--- my_silant/api/views.py
+++ my_silant/api/views.py
@@ -1,8 +1,10 @@
 
 from silant.models import Car, Maintenance, Complaints
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, generics
 from .serializers import CarSerializer, MaintenanceSerializer, ComplaintsSerializer, MiniCarSerializer
 
+from rest_framework.permissions import IsAuthenticated
+from .serializers import ComplaintsSerializer
 
 class CarViewSet(viewsets.ModelViewSet):
     queryset = Car.objects.all().order_by('shipping_date')
@@ -13,47 +15,46 @@ class CarViewSet(viewsets.ModelViewSet):
         else:
             return CarSerializer # если не авторизован отобразить полный список
 
-# Для настройки видимости полей с использованием API в views.py в зависимости от прав, следует использовать декораторы доступа к представлениям (permissions). Эти декораторы позволяют задавать различные права доступа к представлениям в вашем API на основе аутентификации и авторизации пользователей.
+# class MaintenanceViewSet(viewsets.ModelViewSet):
+#     queryset = Maintenance.objects.all().order_by('maintenance_date')
+#     serializer_class = MaintenanceSerializer
+#     permission_classes = [permissions.IsAuthenticated] # Отображение значений только авторизованным
 
-# Пример:
-
-# from rest_framework import permissions
-
-# class MyViewSet(viewsets.ModelViewSet):
-#     queryset = MyModel.objects.all()
-#     serializer_class = MySerializer
-
-#     permission_classes = [permissions.IsAuthenticated]
-
-#     def get_serializer_class(self):
-
-
-#         """Override serializer for fields visibility based on user permission.""" 
-#           Переопределить сериализатор для видимости полей на основе разрешения пользователя
-
-
-#         if not self.request.user.is_authenticated:
-#             return AnonymousSerializer
-
-#         # get the serializer based on the user permission
-#           получить сериализатор на основе разрешения пользователя
-
-#         if self.request.user.has_perm('myapp.view_all_fields'):
-#             return AllFieldsSerializer
-#         else:
-#             return FilteredFieldsSerializer
-
-# В этом примере мы использовали декораторы доступа к представлениям, чтобы указать, что доступ к представлению должен быть только для аутентифицированных пользователей (permissions.IsAuthenticated).
-# Далее мы определили метод get_serializer_class () для определения сериализатора в зависимости от прав доступа пользователя. То есть, если пользователь имеет разрешение 'myapp.view_all_fields', мы используем сериализатор AllFieldsSerializer, который показывает все поля модели. В противном случае мы используем сериализатор FilteredFieldsSerializer, который показывает только определенные поля модели, доступные для этого пользователя.
-# Это далеко не единственный способ конфигурации видимости полей с использованием API в views.py, но он является довольно распространенным и эффективным.
-
-class MaintenanceViewSet(viewsets.ModelViewSet):
-    queryset = Maintenance.objects.all().order_by('maintenance_date')
+class MaintenanceList(generics.ListAPIView):
     serializer_class = MaintenanceSerializer
-    permission_classes = [permissions.IsAuthenticated] # Отображение значений только авторизованным
+    permission_classes = [IsAuthenticated]
 
-class ComplaintsViewSet(viewsets.ModelViewSet):
-    queryset = Complaints.objects.all().order_by('date_of_refusal')
-    serializer_class = ComplaintsSerializer
-    permission_classes = [permissions.IsAuthenticated] # Отображение значений только авторизованным
+    def get_queryset(self):
+        user = self.request.user
+        if user.has_perm('silant.view_maintenance_noclient') == False:
+            queryset = Maintenance.objects.filter(car__client__user=user)
+        elif user.has_perm('silant.view_maintenance_noservice') == False:
+            queryset = Maintenance.objects.filter(service_company__user=user)
+        elif user.has_perm('silant.add_car') == True:
+            queryset = Maintenance.objects.all()
+        else:
+            queryset = Maintenance.objects.none()
+        return queryset
     
+# class ComplaintsViewSet(viewsets.ModelViewSet):
+#     queryset = Complaints.objects.all().order_by('date_of_refusal')
+#     serializer_class = ComplaintsSerializer
+#     permission_classes = [permissions.IsAuthenticated] # Отображение значений только авторизованным
+
+
+class ComplaintsList(generics.ListAPIView):
+    serializer_class = ComplaintsSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.has_perm('silant.view_complaints_noclient') == False:
+            queryset = Complaints.objects.filter(car__client__user=user)
+        elif user.has_perm('silant.view_complaints_noservice') == False:
+            queryset = Complaints.objects.filter(service_company__user=user)
+        elif user.has_perm('silant.add_car') == True:
+            queryset = Complaints.objects.all()
+        else:
+            queryset = Complaints.objects.none()
+        return queryset
+
